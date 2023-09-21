daef63277ed9c8a224c6f211c84eab64485e8257
Yana_Krasnopolskaya <krasnopolskaya.yana331@gmail.com>
2023-09-21 16:16:49 +0700

    add folder Task09-16

---
+++ Task09/Program.cs
@@ -0,0 +1,37 @@
+﻿// 9. Напишите программу, которая 
+// 1. выводит случайное число из отрезка [10, 99] и 
+// 2. показывает наибольшую цифру числа.
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+
+
+int numberToDigit = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из отрезка от 10 до 99: {numberToDigit}");
+
+// int firstDigit = numberToDigit / 10;
+// int secondDigit = numberToDigit % 10;
+
+// if (firstDigit > secondDigit) 
+// {
+//     Console.WriteLine($"Наибольшая цифра числа: {firstDigit}");
+// }
+// else
+// {
+//     Console.WriteLine($"Наибольшая цифра числа: {secondDigit}");
+// }
+
+// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit; тернарный оператор(возвращается правда или ложь)
+                                        // ? - тогда    : - иначе
+// Console.WriteLine($"Наибольшая цифра числа: {maxDigit}");
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
+
+int maxDigit = MaxDigit(numberToDigit);
+Console.WriteLine($"Наибольшая цифра числа: {maxDigit}");
---
+++ Task10/Program.cs
@@ -0,0 +1,23 @@
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int DeleteSecondDigit(int number)
+{
+    int firstDigit = number / 100;
+    int thirdDigit = number % 10;
+    return firstDigit * 10 + thirdDigit;
+}
+
+int numberToDigit = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число из отрезка от 100 до 999: {numberToDigit}");
+
+// int firstDigit = numberToDigit / 100;
+// int thirdDigit = numberToDigit % 10;
+// int result = firstDigit * 10 + thirdDigit;
+
+int result = DeleteSecondDigit(numberToDigit);
+Console.WriteLine($"Итоговое число при удалении второго числа:  {result}");

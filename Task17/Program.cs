369700d9a3f56f050ad04b14470e4f07dfb9c264
Yana_Krasnopolskaya <krasnopolskaya.yana331@gmail.com>
2023-09-25 18:48:04 +0700

    add Task 17,18,21,22

---
+++ Task17/Program.cs
@@ -0,0 +1,32 @@
+﻿// 17. Напишите программу, которая принимает на вход
+// координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт
+// номер четверти плоскости, в которой находится эта
+// точка.
+
+
+int GetUserInput(string str)
+{
+    Console.WriteLine(str);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int Quater(int x, int y)
+{
+    if (x > 0 && y > 0) return 1;
+    if (x < 0 && y > 0) return 2;
+    if (x < 0 && y < 0) return 3;
+    if (x > 0 && y < 0) return 4;
+    return 0;
+}
+
+int xCoordinate = GetUserInput("Введите координаты точки X: ");
+int yCoordinate = GetUserInput("Введите координаты точки Y: ");
+
+int quater = Quater(xCoordinate, yCoordinate);
+
+string result = quater > 0 
+                ? $"Указанные координаты соответствуют четверти {quater}"
+                : "Введены некорректные координаты";
+
+Console.WriteLine(result);
---
+++ Task18/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 18: Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+int GetUserInput(string message)
+{
+    Console.Write(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+string RangeCoordinate(int quarterNumber)
+{
+    if (quarterNumber == 1) return "x > 0 и y > 0";
+    if (quarterNumber == 2) return "x < 0 и y > 0";
+    if (quarterNumber == 3) return "x < 0 и y < 0";
+    if (quarterNumber == 4) return "x > 0 и y < 0";
+    return null;
+}
+
+int rangecoordinate = GetUserInput("Введите номер четверти: ");
+string result = RangeCoordinate(rangecoordinate);
+Console.WriteLine($"Диапазон возможных координат точек в этой четверти: {result}");

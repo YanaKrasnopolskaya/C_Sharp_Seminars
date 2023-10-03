834e1367e87706100662e89207f9d9f42c7ee354
Yana_Krasnopolskaya <krasnopolskaya.yana331@gmail.com>
2023-10-03 10:30:41 +0700

    Chenge Task29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -2,31 +2,49 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
-int[] RandomArray(int arrLength)
+int[] RandomArray(int size, int min, int max)
 {
-    int[] arr = new int[arrLength];
-    for (int i = 0; i < arrLength; i++)
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
     {
-        arr[i] = new Random().Next(1,100);
+        arr[i] = rnd.Next(min, max + 1);
     }
     return arr;
 }
 
+//void PrintArray(int[] arr)
+//{
+    
+    // Console.Write($"{string.Join(", ", arr)} --> [");      srtring.Join - метод для склеивания элементов массива с
+    // for (int i = 0; i < arr.Length; i++)                    заданным разделителем.
+    // {
+    //     if (i < arr.Length - 1)
+    //     {                                                       //var str = string.Join(", ", arr);
+    //         Console.Write($"{arr[i]}, ");                         var - тип, который даёт компилятору самому выбрать тип данных
+    //     }                                                         для str.
+    //     else
+    //     {                       
+    //         Console.Write($"{arr[i]}]");
+    //     }
+    // }
+//}
+
 void PrintArray(int[] arr)
 {
-    //var str = string.Join(", ", arr);
-    Console.Write($"{string.Join(", ", arr)} --> [");
     for (int i = 0; i < arr.Length; i++)
     {
-        if (i < arr.Length - 1)
+        if (i <  arr.Length - 1)
         {
             Console.Write($"{arr[i]}, ");
         }
-        else
-        {
-            Console.Write($"{arr[i]}]");
-        }
+        else Console.Write($"{arr[i]}");
     }
+    
 }
-int[] array = RandomArray(8);
+
+int[] array = RandomArray(8, 1, 10);
 PrintArray(array);
+Console.Write($" --> [");
+PrintArray(array);
+Console.Write("]");

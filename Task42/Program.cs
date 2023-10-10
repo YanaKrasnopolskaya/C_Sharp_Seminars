bb579efd7304195ee84537420cffddef99b64732
Yana_Krasnopolskaya <krasnopolskaya.yana331@gmail.com>
2023-10-10 10:21:08 +0700

    Task42 +

---
+++ Task42/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 42: 
+// Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+
+
+
+int GetUserInput(string message)
+{
+    Console.Write(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    
+    return num;
+}
+
+string BinaryCalc(int num)
+{
+   string res = string.Empty;
+
+   while (num > 0)
+   {
+        res = num % 2 + res;
+        num /= 2;
+   }
+   return res;
+} 
+
+// int DecimalToBinaryInt(int number, int baseNum)
+// {
+//     int result = 0;
+//     int mult = 1;
+
+//     while (number > 0)                                  // БЕЗ STRING!!!!!
+//     {
+//         result = (number % baseNum) * mult + result;
+//         mult *= 10;
+//         number /= baseNum;
+//     }
+//     return result;
+// }
+
+int number = GetUserInput("Введите число в десятичной системе: ");
+string result = BinaryCalc(number);
+Console.WriteLine($"{number} --> {result}");

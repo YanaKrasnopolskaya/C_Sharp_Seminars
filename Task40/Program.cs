9d34b29d558984e6d9a7404c4cc2bc77e96ab959
Yana_Krasnopolskaya <krasnopolskaya.yana331@gmail.com>
2023-10-10 10:20:42 +0700

    Task40 +

---
+++ Task40/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 40: 
+// 1. Напишите программу, которая принимает на вход три числа 
+// 2. проверяет, может ли существовать треугольник с сторонами такой длины.
+
+
+int GetUserInput(string message)
+{
+    Console.Write(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+bool TriangleExistence(int a, int b, int c)
+{
+    return a + b > c && a + c > b && b + c > a;
+}
+
+int sideA = GetUserInput("Введите длинну первой стороны: ");
+int sideB = GetUserInput("Введите длинну второй стороны: ");
+int sideC = GetUserInput("Введите длинну третьей стороны: ");
+
+
+if (TriangleExistence(sideA, sideB, sideC))
+{
+    Console.WriteLine("Треугольник с заданными значениями существует!");
+}
+else Console.WriteLine("Треугольник с заданными значениями не существует!");

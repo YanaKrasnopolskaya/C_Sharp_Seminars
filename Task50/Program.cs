e92d40090df923c50097a8f6b9d2efc9c6795c9f
Yana_Krasnopolskaya <krasnopolskaya.yana331@gmail.com>
2023-10-17 11:01:43 +0700

    Task 50+

---
+++ Task50/Program.cs
@@ -0,0 +1,116 @@
+﻿// Внутри класса Answer напишите методы CreateIncreasingMatrix, 
+// PrintArray, FindNumberByPosition и PrintCheckIfError.
+
+// 1. Метод CreateIncreasingMatrix должен создавать матрицу заданной размерности, 
+// с каждым новым элементом увеличивающимся на определенное число. 
+// Метод принимает на вход три числа (n - количество строк матрицы, 
+// m - количество столбцов матрицы, k - число, на которое увеличивается каждый новый элемент) 
+// и возвращает матрицу, удовлетворяющую этим условиям.
+
+// 2. Метод PrintArray должен выводить на экран сгенерированную методом CreateIncreasingMatrix матрицу.
+
+// 3. Метод FindNumberByPosition принимает на вход сгенерированную матрицу 
+// и числа x и y - позиции элемента в матрице. 
+// Если указанные координаты находятся за пределами границ массива, 
+// метод должен вернуть массив с нулевым значением. 
+// Если координаты находятся в пределах границ, 
+// метод должен вернуть массив с двумя значениями: 
+// значением числа в указанной позиции, а второй элемент должен быть равен 0, 
+// чтобы показать, что операция прошла успешно без ошибок.
+
+// 4. Метод PrintCheckIfError должен принимать результат метода FindNumberByPosition 
+// и числа x и y - позиции элемента в матрице. 
+// Метод должен проверить, был ли найден элемент в матрице по указанным координатам (x и y), 
+// используя результаты из метода FindNumberByPosition. 
+// Если такого элемента нет, вывести на экран "There is no such index". 
+// Если элемент есть, вывести на экран "The number in [{x}, {y}] is {значение}".
+
+using System;
+using System.Reflection;
+
+public class Answer
+{
+    public static void PrintArray(int[,] matrix)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write($"{matrix[i, j]}\t");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public static int[,] CreateIncreasingMatrix(int n, int m, int k)
+    {
+        int[,] matrix = new int[n, m];
+        int count = 1;
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[i, j] = count;
+                count += k;
+            }
+        }
+        return matrix;
+    }
+
+    public static int[] FindNumberByPosition(int[,] matrix, int rowPosition, int columnPosition)
+    {      
+        if (rowPosition < matrix.GetLength(0) && columnPosition < matrix.GetLength(1))
+        {
+            int[] array = new int[2];
+            array[0] = matrix[rowPosition, columnPosition]; array[1] = 0;
+            return array;
+        }                
+        else
+        {
+            int[] array1 = new int[1]; 
+            array1[0] = 0;
+            return array1;
+        } 
+            
+    }
+        
+    public static void PrintCheckIfError(int[] results, int X, int Y)
+    {
+        if(results.Length == 1)
+        {
+          Console.WriteLine("There is no such index");
+        }
+        else Console.WriteLine($"The number in [{X}, {Y}] is {results[0]}");
+        
+    }
+
+
+    static public void Main(string[] args)
+    {
+        int n, m, k, x, y;
+
+        if (args.Length >= 5)
+        {
+            n = int.Parse(args[0]);
+            m = int.Parse(args[1]);
+            k = int.Parse(args[2]);
+            x = int.Parse(args[3]);
+            y = int.Parse(args[4]);
+        }
+        else
+        {
+
+            n = 3;
+            m = 4;
+            k = 2;
+            x = 2;
+            y = 3;
+        }
+
+
+        int[,] result = CreateIncreasingMatrix(n, m, k);
+        PrintArray(result);
+        PrintCheckIfError(FindNumberByPosition(result, x, y), x, y);
+    }
+}

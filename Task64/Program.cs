2aced6a98f4ccb559fc4b334a9706b7a74359dd8
Yana_Krasnopolskaya <krasnopolskaya.yana331@gmail.com>
2023-10-17 14:43:56 +0700

    Change Task 64

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -3,36 +3,56 @@
 // Выполнить с помощью рекурсии.
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
-
-int GetUserInput(string message)
+using System;
+public class Answer
 {
-    Console.Write(message);
-    int num = Convert.ToInt32(Console.ReadLine());
-    return num;
-}
+    /// <summary>
+    /// Метод пользовательского ввода
+    /// </summary>
+    /// <param name="message">сообщение для пользователя</param>
+    /// <returns>целое число</returns>
+    public static int GetUserInput(string message)
+    {
+        Console.Write(message);
+        int num = Convert.ToInt32(Console.ReadLine());
+        return num;
+    }
 
-bool NatularsCheck(int num)
-{
-    if (num < 0) return false;
+    /// <summary>
+    /// Метод проверки на натуральность
+    /// </summary>
+    /// <param name="num">целое число</param>
+    /// <returns>true || false</returns>
+    public static bool NatularsCheck(int num)
+    {
+        if (num < 1) return false;
 
-    else return true;
-}
+        else return true;
+    }
 
-void NaturalsNumber(int num)
-{
-    if (num == 0) return;
+    /// <summary>
+    /// Метод вывода натуральных чисел в промежутке от N до 1
+    /// </summary>
+    /// <param name="num">целое натуральное число</param>
+    public static void NaturalsNumber(int num)
+    {
+        if (num == 0) return;
 
-    Console.Write($"{num}  ");
-    NaturalsNumber(num - 1);
-}
+        Console.Write($"{num}  ");
+        NaturalsNumber(num - 1);
+    }
 
-int number = GetUserInput("Введите натуральное число: ");
-if(NatularsCheck(number))
-{
-    Console.Write($"{number} --> ");
-    NaturalsNumber(number);
-}
-else
-{
-    Console.WriteLine("Вы ввели не натуральное число!");
+    public static void Main(string[] args)
+    {
+        int number = GetUserInput("Введите натуральное число: ");
+
+        if(NatularsCheck(number))
+        {
+            Console.Write($"{number} --> ");
+            NaturalsNumber(number);
+        }
+
+        else Console.WriteLine("Вы ввели не натурально число!");
+    }
+
 }
